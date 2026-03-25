92ee006f5891af49335a7d2b3e9c944cddb56481
Shrey Choksi <shreychoksi83@gmail.com>
2026-03-25 14:16:43 +0530

    MVC_skeletonStructure

--- shared/chatTypes.js
+++
@@ -1,5 +0,0 @@
-export const CHAT_TYPES = {
-  GROUP: "group",
-  BROADCAST: "broadcast",
-  PRIVATE: "private"
-};
--- shared/validation.js
+++
@@ -1,8 +0,0 @@
-export const validateMessage = (msg) => {
-  if (!msg.username || !msg.message) return false;
-  if (!msg.type) return false;
-
-  if (msg.type === "private" && !msg.to) return false;
-
-  return true;
-};
